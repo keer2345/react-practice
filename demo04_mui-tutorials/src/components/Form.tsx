fd561d41c73cabe3ae7a9f835390e40ce066e5aa
keer2345 <keer2345@gmail.com>
2023-04-11 21:16:19 +0800

    demo04 MUI FormControl

--- demo04_mui-tutorials/src/components/Form.tsx
+++ demo04_mui-tutorials/src/components/Form.tsx
@@ -1,11 +1,21 @@
-import { Button, TextField } from '@mui/material'
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField
+} from '@mui/material'
 import { useState } from 'react'
 
 const Form = () => {
   const [inputs, setInputs] = useState({
     name: '',
     email: '',
-    password: ''
+    password: '',
+    terms: false,
+    courses: ''
   })
 
   const handleChange = (e: any) => {
@@ -19,7 +29,7 @@ const Form = () => {
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
           name='name'
           value={inputs.name}
@@ -47,9 +57,17 @@ const Form = () => {
           sx={{ margin: '20px' }}
         />
         <br />
-        <Button type='submit' onClick={handleSubmit}>
-          Submit
-        </Button>
+        <FormControl fullWidth>
+          <InputLabel id='menu'>Courses</InputLabel>
+          <Select name='courses' label='courses' value={inputs.courses} onChange={handleChange}>
+            <MenuItem value={'mongodb'}>Mongodb</MenuItem>
+            <MenuItem value={'express'}>Express</MenuItem>
+            <MenuItem value={'node'}>Node</MenuItem>
+            <MenuItem value={'react'}>React</MenuItem>
+          </Select>
+        </FormControl>
+        <br />
+        <Button type='submit'>Submit</Button>
       </form>
     </>
   )
